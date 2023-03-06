4dbd9a1f09e151ee5a807ca61ef8cb19d8c4ff5e
Crazy Coder <30525263+crazycoder08@users.noreply.github.com>
2023-03-06 16:02:27 +0530

    build

---
+++ build/precache-manifest.4893816c29d0af8b3d8bef6af22501d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e9063f9097c11e5aa6f5cfd3d58ba2",
+    "url": "/CubeMail/index.html"
+  },
+  {
+    "revision": "7107a99d63e8eb251254",
+    "url": "/CubeMail/static/js/2.3a688b5c.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/CubeMail/static/js/2.3a688b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5c4a4554e7e67c0264",
+    "url": "/CubeMail/static/js/main.d66bf6c8.chunk.js"
+  },
+  {
+    "revision": "e4d6d5a8b1e409ffc3ec",
+    "url": "/CubeMail/static/js/runtime-main.8b14a9d4.js"
+  },
+  {
+    "revision": "c9034baf5e3a91a16cd1d3ba9de5b925",
+    "url": "/CubeMail/static/media/empty_email.c9034baf.svg"
+  }
+]);
